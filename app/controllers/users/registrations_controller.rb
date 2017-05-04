60dd89d13e8aa88d5db319ab3c8f163c0224696d
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-05-04 22:31:53 +0200

    registrations done

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,22 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  protected
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
+  def after_update_path_for(resource)
+    user_path(resource)
+  end
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :picture)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :picture, :birth_date, :password, :password_confirmation)
+  end
+end
