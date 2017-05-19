d58f9f78ab315c8d512f91f255325cd0791e7cb7
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-05-19 17:57:09 +0200

    controllers created

---
+++ app/models/upload.rb
@@ -0,0 +1,6 @@
+class Upload < ApplicationRecord
+  belongs_to :service
+
+  validates :photo, presence: true
+  mount_uploader :photo, PhotoUploader
+end
---
+++ db/migrate/20170519154727_create_uploads.rb
@@ -0,0 +1,12 @@
+class CreateUploads < ActiveRecord::Migration[5.0]
+  def change
+    create_table :uploads do |t|
+      t.string :photo
+      t.string :title
+      t.string :description
+      t.references :service, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
