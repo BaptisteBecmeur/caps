27b15790b7a645bf518f0319558a90b3b519649d
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-05-24 17:36:44 +0200

    prestas view done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @services = Service.order('created_at DESC').limit(9)
   end
 end
