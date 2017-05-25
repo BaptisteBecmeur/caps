f7a7511cc1a8f83dbbfbaa0dec9b968054ec26cc
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-05-25 15:38:10 +0200

    ok

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    @services = Service.order('created_at DESC').limit(9)
+    @services = Service.order('created_at DESC').limit(6)
   end
 end
