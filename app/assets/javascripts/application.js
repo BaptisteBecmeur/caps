cd6416ada469ef1d213e17ae028c5e003eaaf0c0
Baptiste Becmeur <baptistebecmeur@gmail.com>
2017-06-16 20:56:31 +0200

    ok

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,19 @@
 //= require gmaps/google
 //= require_tree .
 
+// javascripts/navbar_animation.js
+
+// $(function(e) {
+//    $(window).on("scroll", function(e){
+//     if ($(this).scrollTop() > 400) {
+//       $(".navbar-wagon").css({
+//         "margin-top": "-70px"
+//       });
+//     }
+//     else {
+//       $(".navbar-wagon").css({
+//         "margin-top": "0px"
+//       });
+//     }
+//   });
+// });
--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,8 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :uploads, dependent: :destroy
 
+  validates :address, presence: true
+
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
 
